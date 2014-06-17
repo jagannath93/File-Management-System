b59913307ede17fc8c8d42c7bf98203daa02f873
Jagannath93 <shanmuk.mir@gmail.com>
2014-06-17 10:40:14 +0530

    search fields updated for Book model in admin

--- fms/admin.py
+++ fms/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from fms.models import *
 
 class BookAdmin(admin.ModelAdmin):
-  list_filter = ('name', 'isbn_number', 'category', 'publisher', 'address')
-  search_fields = ['name', 'isbn_number', 'category', 'publisher', 'address']
+  list_filter = ('name', 'code', 'isbn_number', 'author', 'publisher')
+  search_fields = ['name', 'code', 'isbn_number', 'author', 'publisher']
 
 admin.site.register(Category)
 admin.site.register(Book, BookAdmin)
